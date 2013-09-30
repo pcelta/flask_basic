50fbf26fcb96b7d09453bc380b2e3343afbe0c38
Pedro Ribeiro <pedro.silva@titansgroup.com.br>
2013-09-30 19:28:59 -0300

    Added field in interpartner validator

--- src/validators/interpartner/validator.py
+++ src/validators/interpartner/validator.py
@@ -3,17 +3,24 @@ from src.validators.abstract_validator import AbstractValidator
 class Validator(AbstractValidator):
     
     def validate(self, action, converted_json):
-        generic_fields = []   
+        generic_fields = ["partner", "purchase"]
+        
         if action == "activate" :
+        
             mandatories_fields = generic_fields + [
-                "purchase", 
-                "contractId", 
-                "password", 
-                "action", 
-                "customerIdentification"
+                "contractId",
+                "password",
+                "action",
+                "customerIdentification",
+                "planCode",
+                "startDate",
+                "endDate",
+                "callback"
             ]
             
-            return super(Validator, self)._check(converted_json, mandatories_fields)
-            
+            return self._check(converted_json, mandatories_fields)
+
+        if action == "cancel" :
+            pass
         return False
 
