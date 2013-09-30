e26ed0f881a397f4a984d63db30695fce47710d4
Pedro Ribeiro <pedro.silva@titansgroup.com.br>
2013-09-30 20:11:35 -0300

    Created builder for activation interpartner

--- src/builders/interpartner/activate.py
+++ src/builders/interpartner/activate.py
@@ -1,6 +1,18 @@
 #import ipdb; ipdb.set_trace()
 from src.builders.abstract_builder import AbstractBuilder
+from src.entities.content import Content
 
 class ActivateBuilder(AbstractBuilder):
+
     def build(self, order):
-        pass
+        item_data['CodigoContrato']        = order['contractId']
+        item_data['Senha']                 = order['password']
+        item_data['CodigoPlano']           = order['planCode']
+        item_data['CodigoIdentificacao']   = order['customerIdentification']
+        item_data['DataInicio']            = order['startDate']
+        item_data['DataTermino']           = order['endDate']
+        item_data['Status']                = order['action']
+
+        data['Cliente'] = item_data
+
+        return Content(data)
