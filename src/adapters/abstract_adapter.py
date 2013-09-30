de2b809f30a85d9ced3cc40c25717708cbfe87d6
Pedro Ribeiro <pedro.silva@titansgroup.com.br>
2013-09-30 20:06:15 -0300

    Changed for receive partner_settings in constructor

--- src/adapters/abstract_adapter.py
+++ src/adapters/abstract_adapter.py
@@ -1,6 +1,10 @@
 import abc
 
 class AbstractAdapter(object):
+
     @abc.abstractmethod
     def call(self, order):
         pass
+
+    def __init__(self, partner_settings):
+        self._partner_settings = partner_settings
