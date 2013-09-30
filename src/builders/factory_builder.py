3f2d939a1674b883ec6a87f13715a7ede3bc40ad
Pedro Ribeiro <pedro.silva@titansgroup.com.br>
2013-09-30 20:10:35 -0300

    Added loading for response builders

--- src/builders/factory_builder.py
+++ src/builders/factory_builder.py
@@ -12,9 +12,19 @@ class FactoryBuilder(object):
             raise ValueError('Invalid Argument')
 
         partner_settings = Loader.get_partner_settings(partner_name)
+        builders_path = 'src.builders.%s.%s' % partner_name, action
 
-        builders_path = 'src.builders.%s' % partner_name
         if action == 'activate' :
-            module = __import__(builders_path + '.activate', globals(), locals(), ['ActivateBuilder'])
+            module = __import__(builders_path, globals(), locals(), ['ActivateBuilder'])
             return module.ActivateBuilder()
+
+        if action == 'cancel' :
+            module = __import__(builders_path, globals(), locals(), ['CancelBuilder'])
+            return module.CancelBuilder()
+
+        if action == 'response' :
+            module = __import__(builders_path, globals(), locals(), ['ResponseBuilder'])
+            return module.ResponseBuilder()
+
+        raise ValueError('Invalid Argument. Action <%s> unknow' % action)
 
