0f60218b6b327a98ac042534a645a6b37bdb328f
Pedro Ribeiro <pedro.silva@titansgroup.com.br>
2013-09-30 20:09:32 -0300

    Added AbstractBuilderResponse for response builders

--- src/builders/abstract_builder.py
+++ src/builders/abstract_builder.py
@@ -5,3 +5,9 @@ class AbstractBuilder(object):
     @abc.abstractmethod
     def build(self, order):
         pass
+
+class AbstractResponseBuilder(object):
+
+    @abc.abstractmethod
+    def build(self, response):
+        pass
