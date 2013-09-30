66e21e107daea97db58cf325b68e48264ae6fbe0
Pedro Ribeiro <pedro.silva@titansgroup.com.br>
2013-09-30 20:11:05 -0300

    Created initial interpartner adapter

--- src/adapters/interpartner/adapter.py
+++ src/adapters/interpartner/adapter.py
@@ -1,6 +1,11 @@
 from src.adapters.abstract_adapter import AbstractAdapter
+import suds
 
 class Adapter(AbstractAdapter):
 
-    def call(self, order):
-        pass
+    def call(self, content):
+        if content not isinstance Content :
+            raise ValueError('Invalid Argument. Must be Content instance')
+
+        client = suds.client.Client(self._partner_settings['wsdl'])
+        client.service.Incluir()
