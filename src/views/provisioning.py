d881f5b52b6a2c3b15e59a711ee7fea3f2c8526a
Pedro Ribeiro <pedro.silva@titansgroup.com.br>
2013-10-01 19:54:11 -0300

    Added json dumps in final response

--- src/views/provisioning.py
+++ src/views/provisioning.py
@@ -8,7 +8,7 @@ class Provisioning(object):
 
     validator = None
     provisioning_service = None
-    
+
     def __init__(self, validator, provisioning_service) :
         if not isinstance(validator, JsonValidator) :
             raise ValueError('Invalid Argument. Must be JsonValidator instance')
@@ -22,11 +22,13 @@ class Provisioning(object):
     def activate(self, str_json):
         if (self.validator.validate(str_json)) :
             converted_json = json.loads(str_json)
-            return self.provisioning_service.activate(converted_json)
+            response = self.provisioning_service.activate(converted_json)
+            return json.dumps(response)
 
         return '{"error" : "Invalid JSON"}'
 
     @staticmethod
     def create():
-        provisioning_service = ProvisioningService(FactoryBuilder(), FactoryAdapter())
+        builder_factory = FactoryBuilder()
+        provisioning_service = ProvisioningService(builder_factory, FactoryAdapter(builder_factory))
         return Provisioning(JsonValidator(), provisioning_service)
