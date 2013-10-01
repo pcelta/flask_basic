9815bf4313eddd4893ee420d9faef12c42d93ce0
Pedro Ribeiro <pedro.silva@titansgroup.com.br>
2013-10-01 19:50:23 -0300

    Added builder factory dependence

--- src/adapters/factory_adapter.py
+++ src/adapters/factory_adapter.py
@@ -1,18 +1,32 @@
+from configs.loader import Loader
 from configs import config
+from src.builders.factory_builder import FactoryBuilder
 
 class FactoryAdapter(object):
 
-    def create(self, partner_name):
-        module = self._get_adapter_module(partner_name)
-        global_settings = config.get_configs()
-        return module.Adapter(global_settings['partners'][partner_name])
+    _factory = None
 
-    def _get_adapter_module(self, partner_name):
+    def __init__(self, builder_factory):
+        if not isinstance(builder_factory, FactoryBuilder):
+            raise ValueError('Invalid Argument. Must be FactoryBuilder instance')
+        self._factory = builder_factory
+
+
+    def create(self, partner_name):
+
+        settings = Loader.get_partner_settings(partner_name)
+
+        partner_name = self._get_partner_name(partner_name)
+
+        adapters_path = 'src.adapters.%s.adapter' % partner_name
+        adapters_module =  __import__(adapters_path, globals(), locals(), ['Adapter'])
+
+        builder = self._factory.create(partner_name, 'response')
+        return adapters_module.Adapter(settings, builder)
+
+    def _get_partner_name(self, partner_name):
 
         if config.get_env() == 'testing' :
             partner_name = 'mock'
 
-        adapters_path = 'src.adapters.%s.adapter' % partner_name
-        return __import__(adapters_path, globals(), locals(), ['Adapter'])
-
-            
+        return partner_name
