214f995aac162af988fe94d4c4bdac47cdf2947d
Pedro Ribeiro <pedro.silva@titansgroup.com.br>
2013-10-01 19:48:40 -0300

    Added builder response dependece

--- src/adapters/abstract_adapter.py
+++ src/adapters/abstract_adapter.py
@@ -1,10 +1,15 @@
 import abc
+from src.builders.interpartner.response import ResponseBuilder
 
 class AbstractAdapter(object):
+
+    _partner_settings   = None
+    _builder            = None
 
     @abc.abstractmethod
     def call(self, order):
         pass
 
-    def __init__(self, partner_settings):
+    def __init__(self, partner_settings, builder):
         self._partner_settings = partner_settings
+        self._builder = builder
