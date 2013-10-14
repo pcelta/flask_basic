f7533080cc96e5d7fbba609b35f6f6c2be07e813
Pedro Ribeiro <pedro.silva@titansgroup.com.br>
2013-10-14 15:37:10 -0300

    Created builder response fsecure

---
+++ src/builders/fsecure/response.py
@@ -0,0 +1,27 @@
+from src.builders.abstract_builder import AbstractResponseBuilder
+from src.entities.content import Result
+
+class ResponseBuilder(AbstractResponseBuilder):
+
+    _SUCCESS                = 200
+    _PARAMETER_ERROR        = 400
+    _AUTHENTICATION_FAILED  = 401
+    _NO_PERMISSION          = 403
+    _NOT_ALLOWED            = 405
+    _RESOURCE_ERROR         = 500
+
+    def build(self, response):
+
+        result = Result()
+        result.is_success = False
+
+        if response.status_code == self._SUCCESS:
+            result.is_success = True
+
+        elif response.status_code == self._PARAMETER_ERROR:
+            result.message = "Invalid value for customerIdentification or licence parameter"
+        else:
+            # add log
+            pass
+
+        return result
