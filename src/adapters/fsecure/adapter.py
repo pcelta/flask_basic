49c972b20373a651a4d78144585d45a954d40c4c
Pedro Ribeiro <pedro.silva@titansgroup.com.br>
2013-10-14 15:36:48 -0300

    Finish fsecure adapter

--- src/adapters/fsecure/adapter.py
+++ src/adapters/fsecure/adapter.py
@@ -1,10 +1,29 @@
 from src.adapters.abstract_adapter import AbstractAdapter
 from src.entities.content import Content
+from configs.loader import Loader
+import requests
 
 
 class Adapter(AbstractAdapter):
 
+    _action     = None
+    _account    = None
+
+    def set_action(self, action):
+        self._action = action
+
+    def set_account(self, account):
+        self._account = account
+
     def call(self, content):
+
         if not isinstance(content, Content) :
             raise ValueError('Invalid Argument. Must be Content instance')
 
+        settings = Loader.get_partner_settings('FSECURE')
+        authentication_data = settings["authentication"][self._account]
+        endpoint = settings["endpoint_%s" % self._action]
+        url = settings["url"] % self._account
+        response = requests.post(url + endpoint, data=content.data, auth=(authentication_data["user"], authentication_data["password"]))
+
+        return self._builder.build(response)
