da7e5d90f3ea9e18b495a3efe0c9c81b435275a4
Pedro Ribeiro <pedro.silva@titansgroup.com.br>
2013-10-14 15:37:59 -0300

    Created provisioner fsecure

---
+++ src/provisioners/fsecure/provisioner.py
@@ -0,0 +1,23 @@
+from src.provisioners.abstract_provisioner import AbstractProvisioner
+
+from src.entities.content import Result
+
+class Provisioner(AbstractProvisioner):
+
+    def activate(self, order):
+
+        validator = self._factory_validator.create(order['partner'])
+
+        if validator.validate("activate", order) :
+            builder = self._factory_builder.create(order['partner'], 'activate')
+            adapter = self._factory_adapter.create(order['partner'])
+            adapter.set_action("activate")
+            adapter.set_account(order["account"])
+
+            return adapter.call(builder.build(order))
+
+        return Result.create_with_partner_missing_error(validator.get_missing_field())
+
+
+
+
